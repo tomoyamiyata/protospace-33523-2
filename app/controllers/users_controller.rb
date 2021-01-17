25404d3f4a79978c3a3b370e94099f22843d3253
tomoyamiyata <tomoya.0310mdesu@gmail.com>
2021-01-17 13:34:41 +0900

    プロトタイプ関連を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController  
+
+  def index
+
+  end
+
+  def new
+    @user = User.new 
+  end
+
+  def edit
+  end
+
   def show
     @user = User.find(params[:id])
+    @prototypes =  @user.prototypes
   end
-end
+
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
